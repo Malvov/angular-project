be0cbb5a0c69047ca28fc8868026d1294a949d54
malvov <manuel.olivaresbonilla@gmail.com>
2018-07-19 21:46:47 -0500

    listing projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
+  public projects: Array<Project>;
 
-  constructor() { }
+  constructor(
+    private _projectService: ProjectService
+  ) { }
 
   ngOnInit() {
+    this.getProjects();
   }
+
+  getProjects() {
+    this._projectService.getProjects().subscribe(
+      response => {
+        console.log(response);
+        if(response.projects) {
+          this.projects = response.projects;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+
 
 }
